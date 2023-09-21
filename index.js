f0f6e0a60b55f3978f88d9dfbe4a38b4dd567ce6
ogius <kowalewski.olgierd@gmail.com>
2023-09-21 11:11:15 +0200

    Added order of operations

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const inputForm = document.getElementById("input");
 const resultHolder = document.getElementById("result");
 
-const inputRegex = /[0-9*\/\+\-\^]/;
+// const inputRegex = /[0-9*\/\+\-\^\\]/;
+const inputRegex = /[0-9*\/\+\-\^\√]/;
 const numbersRegex = /[0-9]/;
-const actionRegex = /[*\/\+\-\^]/;
+const actionRegex = /[*\/\+\-\^\√]/;
 
 function onInput() {
   let endRes = "";
@@ -15,36 +16,63 @@ function onInput() {
   inputForm.value = endRes;
 }
 
+function performCalculation(character, x, y) {
+  let res = x;
+
+  console.log(character);
+  switch (character) {
+    case "*":
+      res *= y;
+      break;
+    case "/":
+      res /= y;
+      break;
+    case "+":
+      res += y;
+      break;
+    case "-":
+      res -= y;
+      break;
+    case "^":
+      res = Math.pow(res, y);
+      break;
+    case '√':
+      res = Math.pow(y, 1 / res);
+      break;
+  }
+  return res;
+}
 
 function calc() {
-  const numbers = String(inputForm.value).split(actionRegex).map(e => Number(e));
-  const actions = String(inputForm.value).split(numbersRegex).filter(e => e !== "");
+  let numbers = String(inputForm.value).split(actionRegex).map(e => Number(e));
+  let actions = String(inputForm.value).split(numbersRegex).filter(e => e !== "");
 
 
   if (actions.filter(e => e.length !== 1).length) return alert("wrong data");
 
   let sum = numbers[0];
-  numbers.shift();
 
-  actions.map((e, i) => {
-    switch (e) {
-      case "*":
-        sum *= numbers[i];
+  while (numbers.length > 1) {
+    const actionsOrder = "√^/*+-";
+    let activeAction = "";
+    for (let index = 0; index < actionsOrder.length; index++) {
+      if (actions.includes(actionsOrder[index])) {
+        activeAction = actionsOrder[index];
         break;
-      case "/":
-        sum /= numbers[i];
-        break;
-      case "+":
-        sum += numbers[i];
-        break;
-      case "-":
-        sum -= numbers[i];
-        break;
-      case "^":
-        sum = Math.pow(sum, numbers[i]);
-        break;
+      }
     }
-  });
+
+    for (let i = 0; i < actions.length; i++) {
+      const action = actions[i];
+      if (action === activeAction) {
+        numbers[i] = performCalculation(action, numbers[i], numbers[i + 1]);
+        numbers = numbers.filter((_, ii) => ii !== i + 1);
+        actions = actions.filter((_, ii) => ii !== i);
+        break;
+      }
+    }
+  }
+  sum = numbers[0];
 
   resultHolder.textContent = sum;
 }
